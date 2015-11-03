a6193281e419a7846ccbca95102619d37834a05d
masterUNG <phrombutr@gmail.com>
2015-11-03 16:22:55 +0700

    Order Activity

--- app/src/main/java/appewtc/masterung/krurestaurant/OrderActivity.java
+++ app/src/main/java/appewtc/masterung/krurestaurant/OrderActivity.java
@@ -2,12 +2,21 @@ package appewtc.masterung.krurestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView showOfficerTextView;
+    private Spinner deskSpinner;
+    private ListView foodListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-    }
-}
+    }   // Main Method
+
+}   // Main Class
